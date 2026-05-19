06f3583470fcc6e387fd41d11bffa068e481a9fa
codesquadz1606-git <codesquadz1606@gmail.com>
2026-05-19 10:18:10 +0530

    Class 2

---
+++ JS/Class 2/script.js
@@ -0,0 +1,17 @@
+// console.log("External")
+
+// let a=parseInt(prompt("Enter a:"))
+// let b=parseInt(prompt("Enter b:"))
+
+// console.log(a+b)
+
+console.log(Math.PI)
+console.log(Math.abs(-100))
+console.log(Math.random())
+console.log(Math.ceil(10.3))
+console.log(Math.floor(12.9))
+console.log(Math.max(10,20,30,40))
+console.log(Math.min(10,20,30,40))
+
+console.log(Math.ceil(Math.random()*59))
+console.log(Math.floor(Math.random()*59)+1)
